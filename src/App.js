543df49fb4dfa1156427ea38176269aba0bdcf2d
Rebecca Skladd <rebecca.skladd@gmail.com>
2021-02-06 15:27:13 -0500

    Navigation and Logo components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 function App() {
@@ -5,8 +8,8 @@ function App() {
         <div className="App">
             <Navigation />
             <Logo />
-            <ImageLinkForm />
-            <FaceRecognition />
+            {/*<ImageLinkForm />
+            <FaceRecognition />*/}
         </div>
     );
 }
