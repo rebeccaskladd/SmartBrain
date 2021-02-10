3d6565abeef885bbf6ffe6360bdc1536ee88d37f
Rebecca Skladd <rebecca.skladd@gmail.com>
2021-02-10 17:30:13 -0500

    face detection api working

--- src/App.js
+++ src/App.js
@@ -29,21 +29,20 @@ class App extends React.Component {
         super();
         this.state = {
             input: '',
+            imageUrl: ''
         }
     }
 
     onInputChange = (event) => {
-        console.log(event.target.value);
+        this.setState({ input: event.target.value });
     }
 
     onButtonSubmit = () => {
-        console.log('click');
-        app.models.predict(
-            "a403429f2ddf4b49b307e318f00e528b",
-            "https://samples.clarifai.com/face-det.jpg")
+        this.setState({ imageUrl: this.state.input });
+        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
             .then(
                 function (response) {
-                    console.log(response);
+                    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
                 },
                 function (err) {
 
@@ -62,7 +61,7 @@ class App extends React.Component {
                 <Logo />
                 <Rank />
                 <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-                <FaceRecognition />
+                <FaceRecognition imageUrl={this.state.imageUrl} />
             </div>
         );
     }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl }) => {
     return (
-
+        <div className='center ma'>
+            <div className='absolute mt2'>
+                <img src={imageUrl} alt='' width='500px' height='auto' />
+            </div>
+        </div>
     );
 }
 
