39a034248fc9769275588a8fa26b87315cade823
Rebecca Skladd <rebecca.skladd@gmail.com>
2021-02-09 20:19:11 -0500

    clarifai face detection api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
+
+const app = new Clarifai.App({
+    apiKey: '298963b9d64d41f8a18f887931e81a30'
+});
 
 const particlesOptions = {
     particles: {
@@ -18,20 +24,48 @@ const particlesOptions = {
     }
 };
 
-function App() {
-    return (
-        <div className="App">
-            <Particles
-                className='particles'
-                params={particlesOptions}
-            />
-            <Navigation />
-            <Logo />
-            <Rank />
-            <ImageLinkForm />
-            {/*<FaceRecognition />*/}
-        </div>
-    );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+        }
+    }
+
+    onInputChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    onButtonSubmit = () => {
+        console.log('click');
+        app.models.predict(
+            "a403429f2ddf4b49b307e318f00e528b",
+            "https://samples.clarifai.com/face-det.jpg")
+            .then(
+                function (response) {
+                    console.log(response);
+                },
+                function (err) {
+
+                }
+            );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Particles
+                    className='particles'
+                    params={particlesOptions}
+                />
+                <Navigation />
+                <Logo />
+                <Rank />
+                <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+                <FaceRecognition />
+            </div>
+        );
+    }
 }
 
 export default App;
